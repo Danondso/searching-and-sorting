3a68c156633b7428c466dad47836bb244ab61b1d
Dublin Anondson <Stratophone2112@gmail.com>
2019-05-28 07:29:11 -0500

    feat: implemented binary search.

--- src/search/BinarySearch.java
+++ src/search/BinarySearch.java
@@ -1,4 +1,16 @@
 package search;
 
 public class BinarySearch {
+
+    public static int binarySearch(int numToSearchBy, int[] array, int low, int high){
+        if(high >= low) {
+            int middle = low + (high - low) / 2;
+            if (array[middle] == numToSearchBy)
+                return middle;
+            else if (numToSearchBy > array[middle])
+                return binarySearch(numToSearchBy, array, middle + 1, high);
+            else return binarySearch(numToSearchBy, array, low, middle - 1);
+        }
+        return -1;
+    }
 }
